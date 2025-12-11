27ee7486401e63d99552d3bbde8d667b7a21b70a
Azka Muhammad <62870999+azkamuhamco@users.noreply.github.com>
2025-12-11 09:51:58 +0700

    MVC lebih rapi

--- src/main/java/ui/ft/ccit/faculty/transaksi/pemasok/PemasokRepository.java
+++
@@ -1,6 +0,0 @@
-package ui.ft.ccit.faculty.transaksi.pemasok;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PemasokRepository extends JpaRepository<Pemasok, String> {
-}
